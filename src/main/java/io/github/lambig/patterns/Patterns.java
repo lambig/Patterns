ed874aefbdbd02692c748f85a9b6644bb44da316
Lambig <ceolnua51@gmail.com>
2023-08-11 21:11:56 +0900

    Nullセーフ化対応

--- src/main/java/io/github/lambig/patterns/Patterns.java
+++ src/main/java/io/github/lambig/patterns/Patterns.java
@@ -1,17 +1,18 @@
 package io.github.lambig.patterns;
 
-import static io.github.lambig.tuplite._2.Tuple2._2mapWith;
-import static io.github.lambig.tuplite._2.Tuple2.tuple;
-import static java.util.stream.Collectors.toList;
-
-
 import io.github.lambig.tuplite._2.Tuple2;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
+import lombok.NonNull;
+import lombok.experimental.StandardException;
+import static io.github.lambig.tuplite._2.Tuple2._2mapWith;
+import static io.github.lambig.tuplite._2.Tuple2.tuple;
+import static java.util.stream.Collectors.toList;
 
 /**
  * 疑似パターンマッチ定義クラス
@@ -22,143 +23,177 @@ import java.util.stream.Stream;
  */
 public class Patterns<K, V> implements Function<K, V> {
 
-  private final List<Tuple2<Predicate<K>, Function<K, V>>> map;
+    private final List<Tuple2<Predicate<K>, Function<K, V>>> mappings;
 
-  public Patterns(List<Tuple2<Predicate<K>, Function<K, V>>> patterns) {
-    this.map = patterns;
-  }
+    public Patterns(List<Tuple2<Predicate<K>, Function<K, V>>> patterns) {
+        this.mappings = patterns;
+    }
 
-  @SafeVarargs
-  public static <S, O> Patterns<S, O> patterns(Tuple2<Predicate<S>, Function<S, O>>... patterns) {
-    return patterns(Stream.of(patterns).collect(toList()));
-  }
+    @SafeVarargs
+    public static <S, O> Patterns<S, O> patterns(Tuple2<Predicate<S>, Function<S, O>>... patterns) {
+        return patterns(Stream.of(patterns).collect(toList()));
+    }
 
-  public static <S, O> Patterns<S, O> patterns(List<Tuple2<Predicate<S>, Function<S, O>>> patterns) {
-    return new Patterns<>(patterns);
-  }
+    public static <S, O> Patterns<S, O> patterns(List<Tuple2<Predicate<S>, Function<S, O>>> patterns) {
+        return new Patterns<>(patterns);
+    }
 
-  /**
-   * キーが最初に該当するパターンの返却値を取得します
-   *
-   * @param key キー
-   * @return 該当するパターンがあればその返却値、なければnull
-   */
-  public V get(K key) {
-    return this.map.stream()
-        .filter(entry -> entry._1().test(key))
-        .findFirst()
-        .map(_2mapWith((ignored, then) -> then.apply(key)))
-        .orElse(null);
-  }
+    /**
+     * キーが最初に該当するパターンの返却値を取得します
+     *
+     * @param key キー
+     * @return 該当するパターンがあればその返却値、なければnull
+     * @throws NoSuchPatternException 該当するパターンがない場合
+     * @throws NullPointerException   該当するパターンがnullを返却した場合
+     */
+    @NonNull
+    public V get(K key) {
+        var pattern = this.mappings.stream()
+                .filter(entry -> entry._1().test(key))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchPatternException("for key: " + key + ". To allow this pattern to return nullable value, consider using Patterns#getOptionally."));
 
-  @Override
-  public V apply(K k) {
-    return this.get(k);
-  }
+        return Optional.ofNullable(pattern._2().apply(key))
+                .orElseThrow(() -> new NullPointerException("Pattern computed null result. To allow this pattern to return nullable value, consider using Patterns#getOptionally."));
+    }
 
-  /**
-   * キーがwhenを満たした場合に返却する値を指定することで
-   * その値を返却する関数を返却します。
-   * 関数でなく返却値を直接設定できます。
-   *
-   * @param value 返却する値
-   * @param <S>   キー型
-   * @param <O>   値型
-   * @return パターンに設定する返却関数(固定値返却)
-   */
-  public static <S, O> Function<S, O> then(O value) {
-    return anything -> value;
-  }
+    /**
+     * キーが最初に該当するパターンの返却値のをOptionalを取得します。
+     * 該当するパターンがない場合やパターンの返却値がnullの場合、
+     * Optionalは空となります。
+     *
+     * @param key キー
+     * @return 該当するパターンがあればその返却値のOptional
+     */
+    @NonNull
+    public Optional<V> getOptionally(K key) {
+        return this.mappings.stream()
+                .filter(entry -> entry._1().test(key))
+                .findFirst()
+                .map(_2mapWith((ignored, then) -> then.apply(key)));
+    }
 
-  /**
-   * キーがwhenを満たした場合に値を返却するsupplierを指定することで
-   * 入力値を無視してその値を返却する関数を返却します。
-   *
-   * @param supplier 返却する値のsupplier
-   * @param <S>      キー型
-   * @param <O>      値型
-   * @return パターンに設定する返却関数
-   */
-  public static <S, O> Function<S, O> thenSupply(Supplier<O> supplier) {
-    return anything -> supplier.get();
-  }
+    /**
+     * キーを、値の代わりに「値のOptional」にmapするFunctionを返します。
+     * @return このPatternをOptionallyにmapするFunction
+     */
+    public Function<K, Optional<V>> optional() {
+        return this::getOptionally;
+    }
 
-  /**
-   * キーがwhenを満たした場合にキーに適用して返却する関数を明示します。
-   * (実はthenApplyを書かなくても直接whenの第2引数に関数を指定すれば動きます
-   *
-   * @param function 適用する関数
-   * @param <S>      キー型
-   * @param <O>      関数の戻り型
-   * @return パターンに設定する返却関数
-   */
-  public static <S, O> Function<S, O> thenApply(Function<S, O> function) {
-    return function;
-  }
+    @Override
+    public V apply(K k) {
+        return this.get(k);
+    }
 
-  /**
-   * パターンを定義します。
-   *
-   * @param when      キーがこのパターンに該当する条件
-   * @param thenApply キーがこのパターンに該当する場合、キーに適用する関数
-   * @param <S>       キー型
-   * @param <O>       関数の戻り型
-   * @return パターン
-   */
-  public static <S, O> Tuple2<Predicate<S>, Function<S, O>> when(Predicate<S> when, Function<S, O> thenApply) {
-    return tuple(when, thenApply);
-  }
+    /**
+     * キーがwhenを満たした場合に返却する値を指定することで
+     * その値を返却する関数を返却します。
+     * 関数でなく返却値を直接設定できます。
+     *
+     * @param value 返却する値
+     * @param <S>   キー型
+     * @param <O>   値型
+     * @return パターンに設定する返却関数(固定値返却)
+     */
+    public static <S, O> Function<S, O> then(O value) {
+        return anything -> value;
+    }
 
-  /**
-   * 引数と等価であることをwhenと定義します。
-   *
-   * @param target キーとの比較対象
-   * @param <S>    キー型
-   * @return 等価判定
-   */
-  public static <S> Predicate<S> equalsTo(S target) {
-    return s -> Objects.equals(s, target);
-  }
+    /**
+     * キーがwhenを満たした場合に値を返却するsupplierを指定することで
+     * 入力値を無視してその値を返却する関数を返却します。
+     *
+     * @param supplier 返却する値のsupplier
+     * @param <S>      キー型
+     * @param <O>      値型
+     * @return パターンに設定する返却関数
+     */
+    public static <S, O> Function<S, O> thenSupply(Supplier<O> supplier) {
+        return anything -> supplier.get();
+    }
 
-  /**
-   * あらゆるキーに該当するパターンを返却します。
-   * デフォルト値の設定に有用です。
-   *
-   * @param thenApply キーがこのパターンに該当する場合、キーに適用する関数
-   * @param <S>キー型
-   * @param <O>       関数の戻り型
-   * @return デフォルトパターン
-   */
-  public static <S, O> Tuple2<Predicate<S>, Function<S, O>> orElse(Function<S, O> thenApply) {
-    return tuple(anything -> true, thenApply);
-  }
+    /**
+     * キーがwhenを満たした場合にキーに適用して返却する関数を明示します。
+     * (実はthenApplyを書かなくても直接whenの第2引数に関数を指定すれば動きます
+     *
+     * @param function 適用する関数
+     * @param <S>      キー型
+     * @param <O>      関数の戻り型
+     * @return パターンに設定する返却関数
+     */
+    public static <S, O> Function<S, O> thenApply(Function<S, O> function) {
+        return function;
+    }
 
-  /**
-   * あらゆるキーに対し例外を送出させます。
-   * 想定外パターンの対応に有用です。
-   *
-   * @param thenApply キーがこのパターンに該当する場合、キーに適用して例外を生成する関数
-   * @param <S>キー型
-   * @param <O>       関数の戻り型
-   * @return 例外処理送出処理
-   */
-  public static <S, O> Tuple2<Predicate<S>, Function<S, O>> orElseThrow(Function<S, RuntimeException> thenApply) {
-    return tuple(anything -> true, input -> {
-      throw thenApply.apply(input);
-    });
-  }
+    /**
+     * パターンを定義します。
+     *
+     * @param when      キーがこのパターンに該当する条件
+     * @param thenApply キーがこのパターンに該当する場合、キーに適用する関数
+     * @param <S>       キー型
+     * @param <O>       関数の戻り型
+     * @return パターン
+     */
+    public static <S, O> Tuple2<Predicate<S>, Function<S, O>> when(Predicate<S> when, Function<S, O> thenApply) {
+        return tuple(when, thenApply);
+    }
 
-  /**
-   * クラスによるパターンマッチを定義します。
-   *
-   * @param clazz     キーがこのパターンに該当する条件となるクラス
-   * @param thenApply キーがこのパターンに該当する場合、clazzクラスにキャストしたキーに適用する関数
-   * @param <S>       キー型
-   * @param <O>       関数の戻り型
-   * @return パターン
-   */
-  public static <S, T extends S, O> Tuple2<Predicate<S>, Function<S, O>> whenMatch(Class<T> clazz, Function<T, O> thenApply) {
-    return tuple(clazz::isInstance, instance -> thenApply.apply(clazz.cast(instance)));
-  }
+    /**
+     * 引数と等価であることをwhenと定義します。
+     *
+     * @param target キーとの比較対象
+     * @param <S>    キー型
+     * @return 等価判定
+     */
+    public static <S> Predicate<S> equalsTo(S target) {
+        return s -> Objects.equals(s, target);
+    }
+
+    /**
+     * あらゆるキーに該当するパターンを返却します。
+     * デフォルト値の設定に有用です。
+     *
+     * @param thenApply キーがこのパターンに該当する場合、キーに適用する関数
+     * @param <S>キー型
+     * @param <O>       関数の戻り型
+     * @return デフォルトパターン
+     */
+    public static <S, O> Tuple2<Predicate<S>, Function<S, O>> orElse(Function<S, O> thenApply) {
+        return tuple(anything -> true, thenApply);
+    }
+
+    /**
+     * あらゆるキーに対し例外を送出させます。
+     * 想定外パターンの対応に有用です。
+     *
+     * @param thenApply キーがこのパターンに該当する場合、キーに適用して例外を生成する関数
+     * @param <S>キー型
+     * @param <O>       関数の戻り型
+     * @return 例外処理送出処理
+     */
+    public static <S, O> Tuple2<Predicate<S>, Function<S, O>> orElseThrow(Function<S, RuntimeException> thenApply) {
+        return tuple(anything -> true, input -> {
+            throw thenApply.apply(input);
+        });
+    }
+
+    /**
+     * クラスによるパターンマッチを定義します。
+     *
+     * @param clazz     キーがこのパターンに該当する条件となるクラス
+     * @param thenApply キーがこのパターンに該当する場合、clazzクラスにキャストしたキーに適用する関数
+     * @param <S>       キー型
+     * @param <O>       関数の戻り型
+     * @return パターン
+     */
+    public static <S, T extends S, O> Tuple2<Predicate<S>, Function<S, O>> whenMatch(Class<T> clazz, Function<T, O> thenApply) {
+        return tuple(clazz::isInstance, instance -> thenApply.apply(clazz.cast(instance)));
+    }
+
+    @StandardException
+    public static class NoSuchPatternException extends RuntimeException {
+    }
+
 }
 
--- src/test/java/io/github/lambig/patterns/PatternsTest.java
+++ src/test/java/io/github/lambig/patterns/PatternsTest.java
@@ -1,5 +1,14 @@
 package io.github.lambig.patterns;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.function.UnaryOperator;
+import java.util.stream.Stream;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.Accessors;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import static io.github.lambig.funcifextension.function.Functions.compositionOf;
 import static io.github.lambig.funcifextension.function.Functions.sequenceOf;
 import static io.github.lambig.patterns.Patterns.equalsTo;
@@ -13,22 +22,12 @@ import static io.github.lambig.patterns.Patterns.when;
 import static io.github.lambig.patterns.Patterns.whenMatch;
 import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.fail;
-
-
-import java.util.List;
-import java.util.function.UnaryOperator;
-import java.util.stream.Stream;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.experimental.Accessors;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PatternsTest {
 
   @Nested
-  class 設定と取得のテスト {
+  class 設定と取得のテスト_get {
     @Test
     void 該当キーに対応する値または適用結果が取得できること() {
       //SetUp
@@ -47,7 +46,7 @@ class PatternsTest {
     }
 
     @Test
-    void 該当キーに対応する値がなければnullが取得できること() {
+    void 該当キーに対応する値がなければNoSuchPatternExceptionが取得できること() {
       //SetUp
       UnaryOperator<Integer> add1 = i -> i + 1;
       Patterns<Integer, String> target =
@@ -57,13 +56,32 @@ class PatternsTest {
               when(i -> i < 0, thenApply(compositionOf(Object::toString, i -> i, add1))));
 
       //Exercise
-      List<String> actual = Stream.of(-1, 0, 1, 2, 3).map(target).collect(toList());
+       var actual = assertThatThrownBy(() -> Stream.of(-1, 0, 1, 2, 3).map(target).collect(toList()));
       //Verify
-      assertThat(actual).containsExactly("0", null, "1", "2", "b");
+      actual
+              .isInstanceOf(Patterns.NoSuchPatternException.class)
+              .extracting("message")
+              .isEqualTo("for key: 0. To allow this pattern to return nullable value, consider using Patterns#getOptionally.");
     }
 
     @Test
-    void 該当キーに対応する値がなければ例外が送出されること() {
+    void orElseThrowで指定した例外がThrowできること() {
+      //SetUp
+      class ExpectedException extends RuntimeException{
+      }
+      Patterns<Integer, String> target =
+              patterns(
+                      when(equalsTo(3), then("b")),
+                      orElseThrow(anything -> new ExpectedException()));
+
+      //Exercise
+      var actual = assertThatThrownBy(() -> Stream.of(-1, 0, 1, 2, 3).map(target).collect(toList()));
+      //Verify
+      actual.isInstanceOf(ExpectedException.class);
+    }
+
+    @Test
+    void 該当キーに対応する値がnullであればNPEが送出されること() {
       //SetUp
       UnaryOperator<Integer> add1 = i -> i + 1;
 
@@ -72,16 +90,17 @@ class PatternsTest {
               when(equalsTo(3), then("b")),
               when(i -> i > 0, thenApply(Object::toString)),
               when(i -> i < 0, thenApply(compositionOf(Object::toString, i -> i, add1))),
-              orElseThrow(v -> new RuntimeException("この値はパターンにありません: " + v)));
+              orElse(anything -> null));
+
 
       //Exercise
-      try {
-        List<String> actual = Stream.of(0).map(target).collect(toList());
-        //Verify
-        fail();
-      } catch (RuntimeException e) {
-        assertThat(e).hasMessage("この値はパターンにありません: 0");
-      }
+      var actual = assertThatThrownBy(() -> Stream.of(0).map(target).collect(toList()));
+
+      //Verify
+      actual
+              .isInstanceOf(NullPointerException.class)
+              .extracting("message")
+              .isEqualTo("Pattern computed null result. To allow this pattern to return nullable value, consider using Patterns#getOptionally.");
     }
 
     @Test
@@ -117,6 +136,93 @@ class PatternsTest {
       List<String> actual = Stream.of(new A("aaa"), new B("bbb"), new C("ccc")).map(target).collect(toList());
       //Verify
       assertThat(actual).containsExactly("it's a plain A.", "it's a B. value: bbb.", "C here. I've got ccc.");
+    }
+  }
+
+  @Nested
+  class 設定と取得のテスト_getOptinally {
+    @Test
+    void 該当キーに対応する値または適用結果が取得できること() {
+      //SetUp
+      Patterns<Integer, String> target =
+              patterns(
+                      when(equalsTo(3), then("b")),
+                      when(equalsTo(4), thenSupply(() -> "c")),
+                      when(i -> i > 0, thenApply(Object::toString)),
+                      when(i -> i < 0, thenApply(sequenceOf((UnaryOperator<Integer>) i -> i + 1, Number::longValue, Object::toString))),
+                      orElse(then("a")));
+
+      //Exercise
+      var actual = Stream.of(-1, 0, 1, 2, 3, 4).map(target.optional()).collect(toList());
+      //Verify
+      assertThat(actual).extracting(Optional::get).containsExactly("0", "a", "1", "2", "b", "c");
+    }
+
+    @Test
+    void 該当キーに対応する値がなければemptyが取得できること() {
+      //SetUp
+      UnaryOperator<Integer> add1 = i -> i + 1;
+      Patterns<Integer, String> target =
+              patterns(
+                      when(equalsTo(3), then("b")),
+                      when(i -> i > 0, thenApply(Object::toString)),
+                      when(i -> i < 0, thenApply(compositionOf(Object::toString, i -> i, add1))));
+
+      //Exercise
+      var actual = target.getOptionally(0);
+      //Verify
+      assertThat(actual).isEmpty();
+    }
+
+    @Test
+    void orElseThrowで指定した例外がThrowできること() {
+      //SetUp
+      class ExpectedException extends RuntimeException{
+      }
+      Patterns<Integer, String> target =
+              patterns(
+                      when(equalsTo(3), then("b")),
+                      orElseThrow(anything -> new ExpectedException()));
+
+      //Exercise
+      var actual = assertThatThrownBy(() -> Stream.of(-1, 0, 1, 2, 3).map(target.optional()).collect(toList()));
+      //Verify
+      actual.isInstanceOf(ExpectedException.class);
+    }
+
+    @Test
+    void 型パターンマッチのテスト() {
+      //SetUp
+      @RequiredArgsConstructor
+      @Accessors(fluent = true)
+      @Getter
+      class A {
+        final String value;
+      }
+      class B extends A {
+        public B(String value) {
+          super(value);
+        }
+      }
+      class C extends A {
+        public C(String value) {
+          super(value);
+        }
+
+        public String say() {
+          return "C here. I've got " + this.value() + ".";
+        }
+      }
+      Patterns<A, String> target =
+              patterns(
+                      whenMatch(B.class, thenApply(b -> "it's a B. value: " + b.value() + ".")),
+                      whenMatch(C.class, thenApply(C::say)),
+                      orElse(then("it's a plain A.")));
+
+      //Exercise
+      var actual = Stream.of(new A("aaa"), new B("bbb"), new C("ccc")).map(target.optional()).collect(toList());
+      //Verify
+      assertThat(actual).extracting(Optional::get).containsExactly("it's a plain A.", "it's a B. value: bbb.", "C here. I've got ccc.");
     }
   }
 }
